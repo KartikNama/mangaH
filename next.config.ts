670c88b50f5e015e82b725a74f82bc58c966fea4
Kartik Nama <kartikna0@gmail.com>
2026-08-01 04:17:33 +0530

    Tested Well

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3033",
+        pathname: "/media/**",
+      },
+      {
         protocol: "https",
         hostname: "media.saudult.xyz",
+        pathname: "/media/**",
       },
       {
         protocol: "https",
